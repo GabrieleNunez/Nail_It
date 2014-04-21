e579c9432adb0f52ef92c561d03cdc3b26fcf88e
P13Darksight <g_m_nunez@live.com>
2014-04-21 14:25:16 -0400

    Initial Commit and release

---
+++ Nail_It/NailItForm.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace Nail_It
+{
+    partial class NailItForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NailItForm));
+            this.inputBrowseButton = new System.Windows.Forms.Button();
+            this.outputBrowseButton = new System.Windows.Forms.Button();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.outputTextBox = new System.Windows.Forms.TextBox();
+            this.executeButton = new System.Windows.Forms.Button();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.maxWidthNumeric = new System.Windows.Forms.NumericUpDown();
+            this.maxHeightNumeric = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.maxWidthNumeric)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.maxHeightNumeric)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // inputBrowseButton
+            // 
+            this.inputBrowseButton.Location = new System.Drawing.Point(13, 8);
+            this.inputBrowseButton.Name = "inputBrowseButton";
+            this.inputBrowseButton.Size = new System.Drawing.Size(75, 23);
+            this.inputBrowseButton.TabIndex = 0;
+            this.inputBrowseButton.Text = "Browse";
+            this.inputBrowseButton.UseVisualStyleBackColor = true;
+            this.inputBrowseButton.Click += new System.EventHandler(this.inputBrowseButton_Click);
+            // 
+            // outputBrowseButton
+            // 
+            this.outputBrowseButton.Location = new System.Drawing.Point(13, 38);
+            this.outputBrowseButton.Name = "outputBrowseButton";
+            this.outputBrowseButton.Size = new System.Drawing.Size(75, 23);
+            this.outputBrowseButton.TabIndex = 1;
+            this.outputBrowseButton.Text = "Browse";
+            this.outputBrowseButton.UseVisualStyleBackColor = true;
+            this.outputBrowseButton.Click += new System.EventHandler(this.outputBrowseButton_Click);
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(94, 11);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(251, 20);
+            this.inputTextBox.TabIndex = 2;
+            this.inputTextBox.Text = "Input Directory";
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.Location = new System.Drawing.Point(94, 40);
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.Size = new System.Drawing.Size(251, 20);
+            this.outputTextBox.TabIndex = 3;
+            this.outputTextBox.Text = "Output Directory";
+            // 
+            // executeButton
+            // 
+            this.executeButton.Location = new System.Drawing.Point(13, 67);
+            this.executeButton.Name = "executeButton";
+            this.executeButton.Size = new System.Drawing.Size(75, 23);
+            this.executeButton.TabIndex = 7;
+            this.executeButton.Text = "Execute";
+            this.executeButton.UseVisualStyleBackColor = true;
+            this.executeButton.Click += new System.EventHandler(this.executeButton_Click);
+            // 
+            // maxWidthNumeric
+            // 
+            this.maxWidthNumeric.Location = new System.Drawing.Point(137, 67);
+            this.maxWidthNumeric.Maximum = new decimal(new int[] {
+            512,
+            0,
+            0,
+            0});
+            this.maxWidthNumeric.Name = "maxWidthNumeric";
+            this.maxWidthNumeric.Size = new System.Drawing.Size(78, 20);
+            this.maxWidthNumeric.TabIndex = 8;
+            this.maxWidthNumeric.Value = new decimal(new int[] {
+            300,
+            0,
+            0,
+            0});
+            // 
+            // maxHeightNumeric
+            // 
+            this.maxHeightNumeric.Location = new System.Drawing.Point(262, 67);
+            this.maxHeightNumeric.Maximum = new decimal(new int[] {
+            512,
+            0,
+            0,
+            0});
+            this.maxHeightNumeric.Name = "maxHeightNumeric";
+            this.maxHeightNumeric.Size = new System.Drawing.Size(83, 20);
+            this.maxHeightNumeric.TabIndex = 9;
+            this.maxHeightNumeric.Value = new decimal(new int[] {
+            300,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(96, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Width";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(221, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Height";
+            // 
+            // NailItForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(359, 97);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.maxHeightNumeric);
+            this.Controls.Add(this.maxWidthNumeric);
+            this.Controls.Add(this.executeButton);
+            this.Controls.Add(this.outputTextBox);
+            this.Controls.Add(this.inputTextBox);
+            this.Controls.Add(this.outputBrowseButton);
+            this.Controls.Add(this.inputBrowseButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "NailItForm";
+            this.Text = "Nail it: Thumnail Resizing Tool";
+            ((System.ComponentModel.ISupportInitialize)(this.maxWidthNumeric)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.maxHeightNumeric)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button inputBrowseButton;
+        private System.Windows.Forms.Button outputBrowseButton;
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.TextBox outputTextBox;
+        private System.Windows.Forms.Button executeButton;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.Windows.Forms.NumericUpDown maxWidthNumeric;
+        private System.Windows.Forms.NumericUpDown maxHeightNumeric;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+
+    }
+}
+
---
+++ Nail_It/NailItForm.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+namespace Nail_It
+{
+    public partial class NailItForm : Form
+    {
+        public NailItForm()
+        {
+            InitializeComponent();
+        }
+
+        private void inputBrowseButton_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                inputTextBox.Text = folderBrowserDialog.SelectedPath;
+
+            }
+        }
+
+        private void outputBrowseButton_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                outputTextBox.Text = folderBrowserDialog.SelectedPath;
+            }
+        }
+
+        private void executeButton_Click(object sender, EventArgs e)
+        {
+            this.Cursor = Cursors.WaitCursor;
+            string[] files = Directory.GetFiles(inputTextBox.Text);
+            
+            int maxWidth = (int)maxWidthNumeric.Value;
+            int maxHeight = (int)maxHeightNumeric.Value;
+            
+            foreach (string file in files)
+            {
+                try
+                {
+                    Image image = Image.FromFile(file);
+
+                    int width = image.Width;
+                    int height = image.Height;
+
+                    float diffX = (float)maxWidth / (float)width;
+                    float diffY = (float)maxHeight / (float)height;
+                    
+                    float aspectRatio = diffY < diffX ? diffY : diffX;
+                    
+                    int newWidth = (int)((width * aspectRatio));
+                    int newHeight = (int)((height * aspectRatio));
+
+                    Bitmap bitmap = new Bitmap(newWidth, newHeight);
+                    Graphics graphics = Graphics.FromImage(bitmap);
+                    graphics.SmoothingMode = SmoothingMode.HighQuality;
+                    graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                    graphics.DrawImage(image, 0, 0, newWidth, newHeight);
+
+                    ImageFormat imgFormat = ImageFormat.Jpeg;
+                    switch (Path.GetExtension(file).ToLower())
+                    {
+                        case ".jpeg":
+                        case ".jpg":
+                            imgFormat = ImageFormat.Jpeg;
+                            break;
+                        case ".png":
+                            imgFormat = ImageFormat.Png;
+                            break;
+                        case ".gif":
+                            imgFormat = ImageFormat.Gif;
+                            break;
+                        default:
+                            imgFormat  = ImageFormat.Bmp;
+                            break;
+                    }
+                    bitmap.Save(Path.Combine(outputTextBox.Text, Path.GetFileName(file)), imgFormat);
+
+                    graphics.Dispose();
+                    bitmap.Dispose();
+                    image.Dispose();
+                }
+                catch { }
+               
+            }
+            this.Cursor = Cursors.Arrow;
+        }
+    }
+}
